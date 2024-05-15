193be9c91a6e91a88b29a6090a44bbaef6c14217
Jose Cardenas <josejcardenas@yahoo.com>
2024-05-15 08:23:58 +0100

    Testing to implement streaming chains

---
+++ test.py
@@ -0,0 +1,48 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
+from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
+
+load_dotenv()
+
+queue = Queue()
+
+class StreamingHandler(BaseCallbackHandler):
+  def on_llm_new_token(self, token: str, **kwargs):
+    queue.put(token)
+
+chat = ChatOpenAI(streaming=True, callbacks=[StreamingHandler()])
+
+prompt = ChatPromptTemplate.from_messages([
+  ("human", "{content}")
+])
+
+class StreamingChain(LLMChain):
+  def stream(self, input):
+    def task():
+        self(input)
+    
+    Thread(target=task).start()
+
+    while True:
+      token = queue.get()
+      yield token
+    # self(input)
+    # yield 'hi'
+    # yield 'there'
+
+chain = StreamingChain(llm=chat, prompt=prompt)
+for output in chain.stream(input={"content":"tell me a joke"}):
+  print(output)
+
+# chain = LLMChain(llm=chat, prompt=prompt)
+
+# for output in chain.stream(input= {"content": "tell me a joke"}):
+#   print(output)
+
+# # messages = prompt.format_messages(content="tell me a joke")
+# # for message in  chat.stream(messages):
+# #   print(message)
