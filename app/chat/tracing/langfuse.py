1509d1d8fcc41276cd616b0d07eaa3c597a04e0e
Jose Cardenas <josejcardenas@yahoo.com>
2024-05-20 17:04:10 +0100

    Text generation tracing with langfuse

--- app/chat/tracing/langfuse.py
+++ app/chat/tracing/langfuse.py
@@ -3,6 +3,7 @@ from langfuse.client import Langfuse
 
 
 langfuse = Langfuse(
-  os.environ['LANGFUSE_PUBLIC_KEY'],
-  os.environ['LANGFUSE_PRIVATE_KEY']
+  os.getenv("LANGFUSE_PUBLIC_KEY"),
+  os.getenv("LANGFUSE_SECRET_KEY"),
+  host="https://prod-langfuse.fly.dev"
 )
